023d6610c5ba555b827b596d4485f69c15f12670
bran214 <61358979+bran214@users.noreply.github.com>
2020-12-11 15:47:30 -0700

    Create Enemy.java

---
+++ src/gamecode/Enemy.java
@@ -0,0 +1,10 @@
+public class Enemy {
+    private String name;
+    private Combat[] attackPattern;
+    private int attackIndex;
+    private int health;
+    private int damage;
+    private String[] oneLiners;
+    private String deathSound;
+    
+}
