9046e67b86699b16ecd202047c82cd670c44f0a9
bran214 <61358979+bran214@users.noreply.github.com>
2020-12-11 15:43:18 -0700

    Create Room.java

---
+++ src/gamecode/Room.java
@@ -0,0 +1,18 @@
+public class Room {
+    private String name;
+    private Movement[] possibleMovements;
+    private Enemy[] enemies;
+    
+    public void import(String filename) {
+        //TODO
+    }
+    
+    @Override
+    public String toString() {
+        //TODO
+    }
+    
+    public void doAction() {
+        //TODO
+    }
+}
