c512775723808583e5d3ba392af685b230b0a8b7
bran214 <61358979+bran214@users.noreply.github.com>
2021-02-01 18:31:54 -0700

    Edited Game.java
    Created a very cool test level.

--- src/gamecode/Game.java
+++ src/gamecode/Game.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Game {
     public static void main(String[] args) {
-        //TODO
+        Scanner input = new Scanner(System.in);
+        System.out.println("--------------------");
+        System.out.println("|                  |");
+        System.out.println("|                   ");
+        System.out.println("|         O         ");
+        System.out.println("|                   ");
+        System.out.println("|                  |");
+        System.out.println("--------------------");
+        System.out.println("ROOM: TestRoom");
+        System.out.println("DESCRIPTION: A simple room to serve as a test");
+        System.out.println("OPTIONS: 1. Move");
+        if (input.nextLine().toLowerCase().equals("move")) {
+            System.out.print("Which direction? (E): "); // the list will display N/S/E/W if all options are available
+            if (input.nextLine().toLowerCase().equals("e")) {
+                System.out.println("COMPLETED");
+                System.exit(0);
+            }
+        }
+        System.out.println("Invalid. Exiting program.");
     }
 }
